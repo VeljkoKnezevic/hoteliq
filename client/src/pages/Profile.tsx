291cad056c2e44533923a323f1815df81c50136e
Nikolina Obradovic <veljkoBitno@gmail.com>
2024-05-02 20:42:34 +0200

    Profile markup done

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { useState } from "react";
+import Header from "../components/Header";
+import { ProfileInfo } from "../types";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [info, setInfo] = useState<ProfileInfo>({
+    firstName: "Veljko",
+    lastName: "Knezevic",
+    email: "veljkoBitno@gmail.com",
+    password: "password",
+  });
+
+  const [editing, setEditing] = useState<boolean>(false);
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    const { firstName, lastName, email, password } = e.target;
+  };
+
+  return (
+    <>
+      <Header />
+
+      <main>
+        <h2>Profile</h2>
+        <button onClick={() => setEditing(true)}>Edit</button>
+        <form onSubmit={handleSubmit} className="grid">
+          <fieldset disabled={editing}>
+            <label htmlFor="firstName">
+              First Name:
+              <input
+                value={info.firstName}
+                type="text"
+                id="firstName"
+                name="firstName"
+              />
+            </label>
+            <label htmlFor="lastName">
+              Last Name:
+              <input
+                value={info.lastName}
+                type="text"
+                id="lastName"
+                name="lastName"
+              />
+            </label>
+            <label htmlFor="email">
+              Email:
+              <input value={info.email} type="text" id="email" name="email" />
+            </label>
+            <label htmlFor="password">
+              Password:
+              <input
+                value={info.password}
+                type="password"
+                id="password"
+                name="password"
+              />
+            </label>
+          </fieldset>
+
+          {editing && <button type="submit">Confirm editing</button>}
+        </form>
+      </main>
+    </>
+  );
 };
 
 export default Profile;
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,1 +1,8 @@
 export type HotelCardVariants = "nearby" | "popular";
+
+export type ProfileInfo = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
