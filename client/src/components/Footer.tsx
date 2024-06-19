78cfae32fffd0a5479dace1b697cb3087676df03
VeljkoKnezevic <veljkoknedla@gmail.com>
2024-06-19 15:34:37 +0200

    Finished hotel card and footer styling mobile

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,14 +2,14 @@ const Footer = () => {
   return (
     <footer className="mt-6 flex w-screen flex-col bg-primary-blue px-6 py-4 text-[#fff]">
       <div className="flex justify-between">
-        <h3>Hotel IQ</h3>
+        <h3 className="text-xl">Hotel IQ</h3>
         <div className="flex items-center gap-3">
-          <img src="icon-facebook.svg" alt="Facebook" />
-          <img src="icon-instagram.svg" alt="Instagram" />
-          <img src="icon-twitter.svg" alt="Twitter" />
+          <img className="h-5 w-5" src="icon-facebook.svg" alt="Facebook" />
+          <img className="h-5 w-5" src="icon-instagram.svg" alt="Instagram" />
+          <img className="h-5 w-5" src="icon-twitter.svg" alt="Twitter" />
         </div>
       </div>
-      <p className="mt-2">
+      <p className="mt-3 text-center text-xs opacity-60">
         &copy; Copyright {new Date().getFullYear()} | All Rights Reserved
       </p>
     </footer>
--- client/src/components/HotelCard.tsx
+++ client/src/components/HotelCard.tsx
@@ -7,30 +7,61 @@ type THotelCard = {
 const HotelCard = ({ variant }: THotelCard) => {
   if (variant == "nearby") {
     return (
-      <div>
-        <img src="/Australia-1.png" alt="" />
-        <div>
-          <h3>The Aston Hotel</h3>
-          <p>Alice Springs NT0870, Australia</p>
-          <p>
-            <span>$165,3</span>/night
+      <section className="rounded-lg">
+        <img className="rounded-t-lg" src="/Australia-1.png" alt="" />
+        <div className="p-3">
+          <div className="flex justify-between">
+            <h3 className="text-sm font-bold text-text-black">
+              The Aston Hotel
+            </h3>
+            <div className="flex gap-2">
+              <img src="star.svg" alt="Star" />
+              <p className="text-sm font-bold text-text-black">5.0</p>
+            </div>
+          </div>
+          <p className="mt-2 text-xs font-medium text-primary-grey">
+            Alice Springs NT0870, Australia
+          </p>
+          <p className="mt-2 text-xs font-medium text-primary-grey">
+            <span className="text-sm font-bold text-secondary-blue">
+              $165,3{" "}
+            </span>
+            /night
           </p>
         </div>
-      </div>
+      </section>
     );
   } else {
     // variant is popular
     return (
-      <div>
-        <img src="/Australia-1.png" alt="" />
-        <div>
-          <h3>The Aston Hotel</h3>
-          <p>Alice Springs NT0870, Australia</p>
-          <p>
-            <span>$165,3</span>/night
+      <section className="flex gap-4 p-3">
+        <img
+          width={84}
+          className="h-[84px] rounded-sm"
+          src="/Australia-1.png"
+          alt=""
+        />
+        <div className="py-1">
+          <div className="flex items-center justify-between gap-4">
+            <h3 className="text-sm font-bold text-text-black">
+              The Aston Hotel
+            </h3>
+            <p className="text-xs font-medium text-primary-grey">
+              <span className="text-sm font-bold text-secondary-blue">
+                $165,3
+              </span>
+              /night
+            </p>
+          </div>
+          <p className="mt-2 text-xs font-medium text-primary-grey">
+            Alice Springs NT0870, Australia
           </p>
+          <div className="mt-2 flex gap-2">
+            <img src="star.svg" alt="Star" />
+            <p className="text-sm font-bold text-text-black">5.0</p>
+          </div>
         </div>
-      </div>
+      </section>
     );
   }
 };
