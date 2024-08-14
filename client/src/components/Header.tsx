a68bb2c84001eff04d1280168b1d9b249f9513ca
VeljkoKnezevic <veljkoknedla@gmail.com>
2024-08-14 18:11:22 +0200

    Changed header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,26 +3,13 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="mx-6 mt-2 flex items-center justify-between pt-6 text-primary-blue md:mx-10 xl:mx-auto  xl:max-w-[1200px] 2xl:max-w-[1440px]">
-      <h1 className="text-2xl lg:text-3xl">Hotel IQ</h1>
+      <Link to="/">
+        <h1 className="text-2xl lg:text-3xl">Hotel IQ</h1>
+      </Link>
 
-      <nav>
-        <ul className="flex gap-3 text-lg lg:gap-4 lg:text-xl">
-          <li>
-            <Link to="/">
-              <button className="hover:underline hover:underline-offset-2">
-                Home
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile/1">
-              <button className="hover:underline hover:underline-offset-2">
-                Profile
-              </button>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Link to={"/profile/1"}>
+        <img src="/profile.svg" alt="Profile" />
+      </Link>
     </header>
   );
 };
