249c0fbe582e2dbd718b40e72a0cba7c88028dae
VeljkoKnezevic <veljkoknedla@gmail.com>
2024-09-03 15:41:06 +0200

    Implemented logout

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const Header = () => {
-  const [LoggedIn, setLoggedIn] = useState<boolean>(false);
+  const { userLogout, userIsAuthenticated } = useAuth();
+
+  const isAuthenticated = userIsAuthenticated();
 
   return (
     <header className="mx-6 mt-2 flex items-center justify-between pt-6 text-primary-blue md:mx-10 xl:mx-auto  xl:max-w-[1200px] 2xl:max-w-[1440px]">
@@ -10,13 +12,15 @@ const Header = () => {
         <h1 className="text-2xl text-primary-blue lg:text-3xl">Hotel IQ</h1>
       </Link>
 
-      {LoggedIn ? (
-        <Link to={"/profile/1"}>
-          <img src="/profile.svg" alt="Profile" />
-        </Link>
-      ) : (
+      {!isAuthenticated && (
         <Link to={"/login"}>
           <img src="/profile.svg" alt="Profile" />
+        </Link>
+      )}
+
+      {isAuthenticated && (
+        <Link to={"/"}>
+          <button onClick={userLogout}>Logout</button>
         </Link>
       )}
     </header>
