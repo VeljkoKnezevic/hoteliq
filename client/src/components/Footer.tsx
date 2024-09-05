445b45261a5f598113a5b3377d3eb945eef0d9d1
VeljkoKnezevic <veljkoknedla@gmail.com>
2024-09-05 19:29:02 +0200

    Added a delete confirmation, styled the buttons

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="sticky top-full flex w-full flex-col overflow-hidden bg-primary-blue px-6 py-4 text-[#fff] md:px-10">
+    <footer className="flex w-full flex-col overflow-hidden bg-primary-blue px-6 py-4 text-[#fff] md:px-10">
       <div className="flex justify-between xl:mx-auto xl:w-full xl:max-w-[1200px]  2xl:max-w-[1440px]">
         <h3 className="text-xl xl:text-2xl">Hotel IQ</h3>
         <div className="flex items-center gap-3">
--- client/src/pages/Staff.tsx
+++ client/src/pages/Staff.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useRef, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 import Header from "../components/Header";
-import { useQuery } from "@tanstack/react-query";
-import { ProfileInfo, THotel } from "../types";
 import HotelCard from "../components/HotelCard";
+import { ProfileInfo, THotel } from "../types";
 
 const Staff = () => {
-  const [search, setSearch] = useState<string>("");
+  const queryClient = useQueryClient();
+
+  const [hotelSearch, setHotelSearch] = useState<string>("");
   const [userSearch, setUserSearch] = useState<string>("");
 
   // Guests
@@ -18,12 +20,46 @@ const Staff = () => {
 
     return await response.json();
   };
-  const guests = useQuery<any[]>({
+  const { data: guestsData } = useQuery<ProfileInfo[]>({
     queryKey: ["guests"],
     queryFn: getGuests,
   });
 
-  const deleteGuest = () => {};
+  const deleteGuest = async (id: number) => {
+    try {
+      const response = await fetch(`http://localhost:8080/guests/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      return await response.json();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteMutation = useMutation({
+    mutationFn: deleteGuest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["guests"] });
+      console.log("Guest deleted successfully");
+    },
+    onError: (error) => {
+      console.error("Error deleting guest:", error);
+    },
+  });
+
+  const handleDeleteClick = (id: number | null) => {
+    if (id) {
+      if (confirm(`Are you sure you want to delete user with id: ${id} `)) {
+        deleteMutation.mutate(id);
+      } else {
+        return;
+      }
+    }
+  };
+
   // Hotels
   const getHotels = async () => {
     const response = await fetch("http://localhost:8080/hotels", {
@@ -34,7 +70,7 @@ const Staff = () => {
     return await response.json();
   };
 
-  const hotels = useQuery<THotel[]>({
+  const { data: hotelsData } = useQuery<THotel[]>({
     queryKey: ["hotels"],
     queryFn: getHotels,
   });
@@ -42,19 +78,22 @@ const Staff = () => {
   const addHotel = () => {};
   const updateHotel = () => {};
   const deleteHotel = () => {};
+
   // Resrevations
   const getReservations = () => {};
   const getReservationsFromGuest = () => {};
 
   return (
-    <div>
+    <>
       <Header />
-      <main className="mx-6 md:mx-10 xl:mx-auto xl:max-w-[1200px] 2xl:max-w-[1440px]">
-        <h2>Staff page</h2>
+      <main className="mx-6 mb-6 md:mx-10 xl:mx-auto xl:max-w-[1200px] 2xl:max-w-[1440px]">
+        <h2 className="mt-3 text-2xl text-secondary-blue underline">
+          Staff page
+        </h2>
 
-        <div>
-          <h3>Guests</h3>
-          <button>Add new</button>
+        {/* Guests */}
+        <div className="mt-6 flex justify-between">
+          <h3 className="text-2xl text-secondary-blue">Guests</h3>
         </div>
         <section>
           <input
@@ -64,39 +103,59 @@ const Staff = () => {
             className=" mb-5 mt-5 w-full rounded-lg border-2 border-secondary-grey bg-[url('/search.svg')] bg-[center_left_0.5rem] bg-no-repeat py-3 pl-8 text-sm font-medium xl:mt-6"
             placeholder="Search Users by Email"
           />
-          {guests.data &&
-            guests.data
-              .filter((guest: ProfileInfo) => guest.email.includes(userSearch))
+          {guestsData &&
+            guestsData
+              .filter((guest: ProfileInfo) =>
+                guest.email?.toLowerCase().includes(userSearch.toLowerCase())
+              )
               .slice(0, 6)
               .map((guest: ProfileInfo) => {
                 return (
-                  <div key={guest.id}>
-                    <div>{guest.email}</div>
+                  <div
+                    className="mb-2 rounded-lg border-2 border-secondary-grey "
+                    key={guest.id}
+                  >
+                    <div className="flex justify-between px-2">
+                      <span className="font-normal">
+                        {guest.firstName ?? "_____"} {guest.lastName ?? "_____"}
+                        , {guest.email}
+                      </span>
+                      <button
+                        onClick={() =>
+                          handleDeleteClick(guest.id ? guest.id : null)
+                        }
+                        className="text-[#f00]"
+                      >
+                        Delete
+                      </button>
+                    </div>
                   </div>
                 );
               })}
         </section>
 
-        <div>
-          <h3>Hotels</h3>
-          <button>Add new</button>
+        {/* Hotels */}
+        <div className="mt-6 flex justify-between">
+          <h3 className="text-2xl text-secondary-blue">Hotels</h3>
+          <button className="rounded-lg bg-secondary-blue px-4 py-1 text-lg text-[#fff]">
+            Add new
+          </button>
         </div>
-
         <section>
           <input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
+            value={hotelSearch}
+            onChange={(e) => setHotelSearch(e.target.value)}
             type="text"
             className=" mb-5 mt-5 w-full rounded-lg border-2 border-secondary-grey bg-[url('/search.svg')] bg-[center_left_0.5rem] bg-no-repeat py-3 pl-8 text-sm font-medium xl:mt-6"
             placeholder="Search Hotel"
           />
           <div>
-            {hotels.data &&
-              hotels.data
+            {hotelsData &&
+              hotelsData
                 .filter((hotel) =>
-                  hotel.name?.toLowerCase().includes(search.toLowerCase())
+                  hotel.name?.toLowerCase().includes(hotelSearch.toLowerCase())
                 )
-                .slice(0, 6)
+                .slice(0, 4)
                 .map((hotel) => {
                   return (
                     <div
@@ -104,20 +163,26 @@ const Staff = () => {
                       key={hotel.id}
                     >
                       <HotelCard data={hotel} variant="popular" />
-                      <button>Edit</button>
-                      <button>Delete</button>
+                      <div className="flex gap-4 p-2">
+                        <button className="rounded  bg-primary-blue p-2 text-base font-medium text-[#fff]">
+                          Edit
+                        </button>
+                        <button className="rounded border-2 border-text-black bg-secondary-grey p-2 text-base font-medium text-[#f00]">
+                          Delete
+                        </button>
+                      </div>
                     </div>
                   );
                 })}
           </div>
         </section>
 
-        <div>
-          <h3>Reservations</h3>
-          <button>Add new</button>
+        {/* Reservations */}
+        <div className="mt-6 flex justify-between">
+          <h3 className="text-2xl text-secondary-blue">Reservations</h3>
         </div>
       </main>
-    </div>
+    </>
   );
 };
 
